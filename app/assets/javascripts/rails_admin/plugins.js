bf445955e065aebfc57e6294efe6867d66a9c82e
Tom Davies <tom@tomdavies.net>
2014-10-13 11:31:23 +0100

    call markitup js

--- app/assets/javascripts/rails_admin/plugins.js
+++ app/assets/javascripts/rails_admin/plugins.js
@@ -2,4 +2,4 @@
 // should override it in your application at javascripts/rails_admin/plugins.js
 // to include javascripts from other gems like:
 //
-// //= require 'rails_admin_markitup/rails_admin_markitup'
+//= require 'rails_admin_markitup/rails_admin_markitup'
